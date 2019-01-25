cc40681f51379149932f04c5a4560e75723d3d59
ken oh <koh@handy.com>
2019-01-24 21:57:06 -0500

    add ant; populate table

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,61 @@
 import React, { Component } from 'react';
 import Header from './Header'
 import Button from './Button'
+// https://ant.design/components/table/
+import { Table } from 'antd';
 import axios from 'axios'
 const dogAPI = 'https://dog.ceo/api/breeds/list/all'
 
+const columns = [{
+  title: 'Name',
+  dataIndex: 'name',
+  key: 'name',
+}, {
+  title: 'Age',
+  dataIndex: 'age',
+  key: 'age',
+}, {
+  title: 'Address',
+  dataIndex: 'address',
+  key: 'address',
+}];
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      dog: {}
+    }
+  }
+
   getDogs = () => {
     axios.get(dogAPI)
       .then(({data}) => {
-        const dogs = data.message
+        const dog = data.message
 
-        this.setState({ dogs })
+        this.setState({ dog })
       })
   }
 
-  setDogState = (dogs) => {
-    // shorthand key-value
-    this.setState({ dogs })
-  }
-
   render() {
+    const dog = this.state.dog
+    const dataSource = Object.keys(dog)
+      .map((x, i) => ({
+        key: x,
+        name: x,
+        age: 3,
+        address: 'here',
+      }))
+
+    const dogTable = dog
+      ? <Table dataSource={dataSource} columns={columns}/>
+      : null
     return (
       <div className="App">
         <Header text={'This is the dog app'} />
         <Button text={'Get dogs'} onClick={this.getDogs}/>
+        { dogTable }
       </div>
     );
   }
