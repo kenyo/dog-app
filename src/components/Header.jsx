6e49e04afe81575b70996572a840b1704c6a76dd
ken oh <koh@handy.com>
2019-01-24 20:42:37 -0500

    add state to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,20 @@ export default class Header extends Component {
   constructor(props) {
     super(props)
 
-    this.state = {foo: 'foo'}
+    this.state = {
+      foo: 'foo',
+      text: this.props.text.slice(0, 10),
+    }
   }
 
   render() {
+    const { foo, text } = this.state
+    const subHead = `${foo} ${text}`
+
     return (
       <div>
         <h1>{this.props.text}</h1>
+        <h2>{subHead}</h2>
         <p>{this.state.foo}</p>
       </div>
     )
