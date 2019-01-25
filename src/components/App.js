eb5d71117cc995b57023e0b04756a953bc2ecbec
ken oh <koh@handy.com>
2019-01-24 21:33:55 -0500

    move axios call from button to root component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 import Header from './Header'
 import Button from './Button'
+import axios from 'axios'
+const dogAPI = 'https://dog.ceo/api/breeds/list/all'
 
 class App extends Component {
+  getDogs = () => {
+    axios.get(dogAPI)
+      .then(({data}) => {
+        const dogs = data.message
+
+        this.setState({ dogs })
+      })
+  }
 
   setDogState = (dogs) => {
     // shorthand key-value
@@ -13,7 +23,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header text={'This is the dog app'} />
-        <Button text={'Get dogs'} setDogState={this.setDogState} />
+        <Button text={'Get dogs'} onClick={this.getDogs}/>
       </div>
     );
   }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,22 +1,9 @@
 import React from 'react';
-import axios from 'axios'
-const dogAPI = 'https://dog.ceo/api/breeds/list/all'
 
 export default function Button(props) {
-  const logDogs = x => console.log(x)
-
-  axios.get(dogAPI)
-    .then(({data}) => {
-      const dogs = data.message
-      props.setDogState(dogs)
-
-      return dogs
-    })
-    .then(logDogs)
-
   return (
     <div>
-      <button onClick={() => console.log('foo')}>{props.text}</button>
+      <button onClick={props.onClick}>{props.text}</button>
     </div>
   )
 }
